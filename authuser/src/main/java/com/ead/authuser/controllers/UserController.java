157bec4434af16032746b84ae920c71b19e0c131
Samuel R Borges <sammy_rb@hotmail.com>
2026-04-25 17:18:25 -0300

    Cria configuração GLOBAL de Pageable

--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -6,8 +6,6 @@ import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -26,8 +24,7 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<UserModel>> getAllUsers( @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC)
-                                                        Pageable pageable) {
+    public ResponseEntity<Page<UserModel>> getAllUsers(Pageable pageable) {
         Page<UserModel> userModelPage = userService.findAll(pageable);
         return ResponseEntity.ok(userModelPage);
     }
