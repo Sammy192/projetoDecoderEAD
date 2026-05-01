a132f440570f5133ecea1bd112a0350e633dc933
Samuel R Borges <sammy_rb@hotmail.com>
2026-04-30 22:02:48 -0300

    Configura carregamento de entidades usando o @Fetch com subselect

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,11 +1,22 @@
 package com.ead.course.repositories;
 
 import com.ead.course.models.ModuleModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+    
+    @EntityGraph(attributePaths = {"course"})
+    ModuleModel findByTitle(String title);
+
+    @Query(nativeQuery = true, value = "select * from tb_modules where course_id = :courseId")
+    List<ModuleModel> findAllModulesByCourseId(@Param("courseId") UUID courseId);
+
 }
