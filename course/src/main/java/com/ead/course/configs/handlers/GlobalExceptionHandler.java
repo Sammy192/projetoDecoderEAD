6df053bdd9981df3d0d2bb2337b3fa0b5d75d616
Samuel R Borges <sammy_rb@hotmail.com>
2026-05-07 22:18:13 -0300

    Cria tratamento para entrada de valores incorretos de Enum

--- course/src/main/java/com/ead/course/configs/handlers/GlobalExceptionHandler.java
+++ course/src/main/java/com/ead/course/configs/handlers/GlobalExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.ead.course.configs.handlers;
 
 import com.ead.course.configs.exceptions.NotFoundException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,4 +36,43 @@ public class GlobalExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Error: Validation failed", errors);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidFormatInputEnumException(HttpMessageNotReadableException ex) {
+        Map<String, String> errors = new HashMap<>();
+        if (ex.getCause() instanceof InvalidFormatException invalidFormatException &&
+                invalidFormatException.getTargetType() != null && invalidFormatException.getTargetType().isEnum()) {
+
+            String fieldName = invalidFormatException.getPath().stream()
+                    .map(JsonMappingException.Reference::getFieldName)
+                    .findFirst().orElse("unknown_field");
+
+            String acceptedValues = Arrays.toString(invalidFormatException.getTargetType().getEnumConstants());
+            String errorMessage = String.format("Invalid value '%s'. Accepted values are: %s", invalidFormatException.getValue(), acceptedValues);
+            errors.put(fieldName, errorMessage);
+
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid input for enum value", errors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+        // Caso seja outro tipo de erro de leitura de JSON (ex: JSON malformado)
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Malformed JSON request. Check the request body structure.", null);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+
+    }
+
+
+    //mais simples sem tratamento na mensagem de erro
+    /*@ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidFormatInputEnumException(HttpMessageNotReadableException ex) {
+        Map<String, String> errors = new HashMap<>();
+        if (ex.getCause() instanceof InvalidFormatException invalidFormatException &&
+                invalidFormatException.getTargetType() != null && invalidFormatException.getTargetType().isEnum()) {
+            String fieldName = invalidFormatException.getPath().get(invalidFormatException.getPath().size()-1).getFieldName();
+            String errorMessage = ex.getMessage();
+            errors.put(fieldName, errorMessage);
+        }
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid input enum value", errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }*/
 }
