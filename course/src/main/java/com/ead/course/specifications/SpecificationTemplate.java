fdefc38de883616c34eb411ddb8e77e7c703c753
Samuel R Borges <sammy_rb@hotmail.com>
2026-05-08 17:51:29 -0300

    Aplica CriteriaBuilder para montar filtros usando o moduleId

--- course/src/main/java/com/ead/course/specifications/SpecificationTemplate.java
+++ course/src/main/java/com/ead/course/specifications/SpecificationTemplate.java
@@ -3,12 +3,15 @@ package com.ead.course.specifications;
 import com.ead.course.models.CourseModel;
 import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Root;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.util.Collection;
 import java.util.UUID;
 
 public class SpecificationTemplate {
