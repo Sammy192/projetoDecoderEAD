a6948dde4588b6910e95f1e2aa8afcec2893a1f3
Samuel R Borges <sammy_rb@hotmail.com>
2026-06-05 15:10:06 -0300

    Configura descoberta de clients com nomes e loadBalance

--- authuser/src/main/java/com/ead/authuser/configs/RestClientConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.ead.authuser.configs;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
 
     @Bean
+    @LoadBalanced
     public RestClient.Builder restClientBuilder() {
         return  RestClient.builder();
     }
